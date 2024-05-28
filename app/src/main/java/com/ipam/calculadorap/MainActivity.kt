6435474c4c4ecbf423c2847bc6080adbb86217c9
bryanmiranda138 <100953022+bryanmiranda138@users.noreply.github.com>
2024-05-28 15:50:14 -0600

    5

--- app/src/main/java/com/ipam/calculadorap/MainActivity.kt
+++ app/src/main/java/com/ipam/calculadorap/MainActivity.kt
@@ -7,12 +7,12 @@ import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    //0->nada; 1->suma; 2->resta; 3->mult; 4->div
+    // 0->nada; 1->suma; 2->resta; 3->mult; 4->div
     var oper: Int = 0
     var numero1: Double = 0.0
     lateinit var tv_num1: TextView
     lateinit var tv_num2: TextView
-    private var isNum2Selected: Boolean = true
+    private var isNum2Selected: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,69 +23,93 @@ class MainActivity : AppCompatActivity() {
         tv_num1 = findViewById(R.id.tv_num1)
         tv_num2 = findViewById(R.id.tv_num2)
 
-        btnIgual.setOnClickListener{
-            var num2 = tv_num2.text.toString().toDouble()
+        btnIgual.setOnClickListener {
+            val num2 = tv_num2.text.toString().toDoubleOrNull() ?: 0.0
             var res = 0.0
-            when(oper){
-                1-> res = numero1 + num2
-                2-> res = numero1- num2
-                3-> res = numero1 * num2
-                4-> res = numero1 / num2
+            when (oper) {
+                1 -> res = numero1 + num2
+                2 -> res = numero1 - num2
+                3 -> res = numero1 * num2
+                4 -> res = numero1 / num2
             }
             tv_num2.setText(res.toString())
             tv_num1.setText("")
+            isNum2Selected = false
         }
 
-        btnBorrar.setOnClickListener{
+        btnBorrar.setOnClickListener {
             tv_num1.setText("")
             tv_num2.setText("")
             oper = 0
-        }
-
-    }
-
-    fun clicNumero(view: View){
-        var num2 = tv_num2.text.toString()
-
-        when(view.id){
-            R.id.btn0 -> tv_num2.setText(num2+"0")
-            R.id.btn1 -> tv_num2.setText(num2+"1")
-            R.id.btn2 -> tv_num2.setText(num2+"2")
-            R.id.btn3 -> tv_num2.setText(num2+"3")
-            R.id.btn4 -> tv_num2.setText(num2+"4")
-            R.id.btn5 -> tv_num2.setText(num2+"5")
-            R.id.btn6 -> tv_num2.setText(num2+"6")
-            R.id.btn7 -> tv_num2.setText(num2+"7")
-            R.id.btn8 -> tv_num2.setText(num2+"8")
-            R.id.btn9 -> tv_num2.setText(num2+"9")
-            R.id.btnPunto -> tv_num2.setText(num2+".")
+            numero1 = 0.0
+            isNum2Selected = false
         }
     }
 
-    fun clicOperacion(view: View){
+    fun clicNumero(view: View) {
         var num2 = tv_num2.text.toString()
-        numero1 = num2.toString().toDouble()
-        tv_num2.setText("")
-        when(view.id){
+
+        when (view.id) {
+            R.id.btn0 -> tv_num2.setText(num2 + "0")
+            R.id.btn1 -> tv_num2.setText(num2 + "1")
+            R.id.btn2 -> tv_num2.setText(num2 + "2")
+            R.id.btn3 -> tv_num2.setText(num2 + "3")
+            R.id.btn4 -> tv_num2.setText(num2 + "4")
+            R.id.btn5 -> tv_num2.setText(num2 + "5")
+            R.id.btn6 -> tv_num2.setText(num2 + "6")
+            R.id.btn7 -> tv_num2.setText(num2 + "7")
+            R.id.btn8 -> tv_num2.setText(num2 + "8")
+            R.id.btn9 -> tv_num2.setText(num2 + "9")
+            R.id.btnPunto -> tv_num2.setText(num2 + ".")
+        }
+    }
+
+    fun clicOperacion(view: View) {
+        val num2 = tv_num2.text.toString()
+        if (num2.isNotEmpty()) {
+            numero1 = num2.toDouble()
+        }
+        when (view.id) {
             R.id.btnSumar -> {
-                tv_num1.setText(num2+"+")
+                if (num2.isNotEmpty()) {
+                    numero1 = num2.toDouble()
+                    tv_num2.setText("")
+                }
+                tv_num1.setText(num2 + "+")
                 oper = 1
+                isNum2Selected = true
             }
-            R.id.btnRestar ->{
-                tv_num1.setText(num2+"-")
+            R.id.btnRestar -> {
+                if (num2.isEmpty()) {
+                    tv_num2.setText("-")
+                } else {
+                    numero1 = num2.toDouble()
+                    tv_num2.setText("")
+                    tv_num1.setText(num2 + "-")
+                }
                 oper = 2
+                isNum2Selected = true
             }
-            R.id.btnMult ->{
-                tv_num1.setText(num2+"*")
+            R.id.btnMult -> {
+                if (num2.isNotEmpty()) {
+                    numero1 = num2.toDouble()
+                    tv_num2.setText("")
+                }
+                tv_num1.setText(num2 + "*")
                 oper = 3
+                isNum2Selected = true
             }
             R.id.btnDividir -> {
-                tv_num1.setText(num2+"/")
+                if (num2.isNotEmpty()) {
+                    numero1 = num2.toDouble()
+                    tv_num2.setText("")
+                }
+                tv_num1.setText(num2 + "/")
                 oper = 4
+                isNum2Selected = true
             }
         }
     }
-
 
     // Llamado cuando se hace clic en el botón de borrar
     fun clicBorrar(view: View) {
@@ -94,14 +118,14 @@ class MainActivity : AppCompatActivity() {
                 tv_num2.text = tv_num2.text.toString().dropLast(1)
             }
             if (tv_num2.text.isEmpty()) {
-                tv_num2.text = "0.0"
+                tv_num2.text = ""
             }
         } else {
             if (tv_num1.text.isNotEmpty()) {
                 tv_num1.text = tv_num1.text.toString().dropLast(1)
             }
             if (tv_num1.text.isEmpty()) {
-                tv_num1.text = "0.0"
+                tv_num1.text = ""
             }
         }
     }
